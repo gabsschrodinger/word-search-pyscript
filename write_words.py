044f3da483ff9f4ebf1357884bb3a2a6a39a0fd1
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2023-06-16 15:03:31 -0300

    Start to track words

--- write_words.py
+++ write_words.py
@@ -3,6 +3,8 @@ from typing import List
 
 written_words = []
 
+def clean_up_words():
+    written_words.clear()
 
 def write_word_horizontally(letter_grid: List[List[dict]], word: str, grid_x: int = None, grid_y: int = None) -> List[List[dict]]:
     new_grid = letter_grid.copy()
@@ -12,6 +14,7 @@ def write_word_horizontally(letter_grid: List[List[dict]], word: str, grid_x: in
     starting_width = random.randint(
         0, len(letter_grid[0]) - 1 - len(word)) if grid_x == None else grid_x
 
+    coordinates = []
     for letter in word:
         if new_grid[height][starting_width]["locked"] and new_grid[height][starting_width]["value"] != letter:
             raise Exception("Letter is locked")
@@ -21,9 +24,18 @@ def write_word_horizontally(letter_grid: List[List[dict]], word: str, grid_x: in
             "locked": True
         }
         new_grid[height][starting_width] = entry
+        coordinates.append((starting_width, height))
         starting_width += 1
 
+    tracked_word = {
+        "word": word,
+        "coordinates": coordinates
+    }
+
+    written_words.append(tracked_word)
+
     return new_grid
+
 
 def write_word_vertically(letter_grid: List[List[dict]], word: str, grid_x: int = None, grid_y: int = None) -> List[List[dict]]:
     new_grid = letter_grid.copy()
@@ -33,6 +45,7 @@ def write_word_vertically(letter_grid: List[List[dict]], word: str, grid_x: int 
     width = random.randint(
         0, len(letter_grid[0]) - 1) if grid_x == None else grid_x
 
+    coordinates = []
     for letter in word:
         if new_grid[starting_height][width]["locked"] and new_grid[starting_height][width]["value"] != letter:
             raise Exception("Letter is locked")
@@ -42,6 +55,14 @@ def write_word_vertically(letter_grid: List[List[dict]], word: str, grid_x: int 
             "locked": True
         }
         new_grid[starting_height][width] = entry
+        coordinates.append((width, starting_height))
         starting_height += 1
+
+    tracked_word = {
+        "word": word,
+        "coordinates": coordinates
+    }
+
+    written_words.append(tracked_word)
 
     return new_grid
--- write_words.test.py
+++ write_words.test.py
@@ -1,10 +1,12 @@
 import unittest
 from letter_grid import generate_random_letter_grid
-from write_words import write_word_horizontally, write_word_vertically
+from write_words import write_word_horizontally, write_word_vertically, written_words, clean_up_words
 import random
 
 
 class TestHorizontalWords(unittest.TestCase):
+    def setUp(self):
+        clean_up_words()
 
     def test_write_word(self):
         test_string = "TEST"
@@ -33,8 +35,26 @@ class TestHorizontalWords(unittest.TestCase):
         self.assertRaises(Exception, write_word_horizontally,
                           letter_grid, exception_string, None, grid_y)
 
+    def test_tracking_word(self):
+        test_string = "TEST"
+        letter_grid = generate_random_letter_grid(10, 10)
+        grid_y = random.randint(0, len(letter_grid) - 1)
+        grid_x = random.randint(0, len(letter_grid[0]) - 1 - len(test_string))
+
+        letter_grid = write_word_horizontally(
+            letter_grid, test_string, grid_x, grid_y)
+
+        self.assertIn(test_string, map(lambda x: x["word"], written_words))
+
+        coordinate_list = list(map(lambda x: (x, grid_y), range(
+            grid_x, grid_x + len(test_string))))
+        tracked_coordinates = list(filter(lambda x: x["word"] == test_string, written_words))[0]["coordinates"]
+        self.assertListEqual(coordinate_list, tracked_coordinates)
+
 
 class TestVerticalWords(unittest.TestCase):
+    def setUp(self):
+        clean_up_words()
 
     def test_write_word(self):
         test_string = "TEST"
@@ -62,5 +82,22 @@ class TestVerticalWords(unittest.TestCase):
 
         self.assertRaises(Exception, write_word_vertically,
                           letter_grid, exception_string, grid_x, None)
+        
+    def test_tracking_word(self):
+        test_string = "TEST"
+        letter_grid = generate_random_letter_grid(10, 10)
+        grid_y = random.randint(0, len(letter_grid) - 1 - len(test_string))
+        grid_x = random.randint(0, len(letter_grid[0]) - 1)
+
+        letter_grid = write_word_vertically(
+            letter_grid, test_string, grid_x, grid_y)
+
+        self.assertIn(test_string, map(lambda x: x["word"], written_words))
+
+        coordinate_list = list(map(lambda y: (grid_x, y), range(
+            grid_y, grid_y + len(test_string))))
+        tracked_coordinates = list(filter(lambda x: x["word"] == test_string, written_words))[0]["coordinates"]
+        self.assertListEqual(coordinate_list, tracked_coordinates)
+
 
 unittest.main()
