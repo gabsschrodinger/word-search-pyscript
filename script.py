6408b88f3db692a2dc397995b2acc4fd98326579
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2023-06-16 15:55:28 -0300

    Convert hamburger menu into close button when menu toggle is turned on

--- script.py
+++ script.py
@@ -12,19 +12,27 @@ letter_grid = generate_random_letter_grid(board_length, board_height)
 def sidebar_toggle():
     if document.querySelector(".sidebar-inactive") == None:
         document.querySelector(".sidebar").classList.add("sidebar-inactive")
-        document.querySelector(
-            ".hamburger").classList.remove('hamburger-active')
         document.querySelector(".shadow").classList.remove("shadow-on")
+        hamburger_container = document.querySelector(
+            ".hamburger")
+        hamburger_container.classList.remove('hamburger-active')
+        hamburger_container.children[0].classList.remove("first-bar")
+        hamburger_container.children[1].classList.remove("middle-bar")
+        hamburger_container.children[2].classList.remove("last-bar")
     elif document.querySelector(".sidebar-inactive") != None:
         document.querySelector(".sidebar").classList.remove("sidebar-inactive")
-        document.querySelector(".hamburger").classList.add("hamburger-active")
         document.querySelector(".shadow").classList.add("shadow-on")
+        hamburger_container = document.querySelector(
+            ".hamburger")
+        hamburger_container.classList.add("hamburger-active")
+        hamburger_container.children[0].classList.add("first-bar")
+        hamburger_container.children[1].classList.add("middle-bar")
+        hamburger_container.children[2].classList.add("last-bar")
 
 
 def shadow_sidebar_toggle():
     if document.querySelector(".shadow-on") != None:
         sidebar_toggle()
-    
 
 
 def rerender_board():
