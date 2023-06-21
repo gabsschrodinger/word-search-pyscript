2e6ba7c82c01f59029aad0b32d41c74fa26e340b
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2023-06-21 16:57:38 -0300

    Refactor css files, create menu page html file

---
+++ python-ui/menu_toggle.py
@@ -0,0 +1,34 @@
+from js import document
+from pyodide.ffi.wrappers import add_event_listener
+
+
+def sidebar_toggle():
+    if document.querySelector(".sidebar-inactive") == None:
+        document.querySelector(
+            ".sidebar").classList.add("sidebar-inactive")
+        document.querySelector(".shadow").classList.remove("shadow-on")
+        hamburger_container = document.querySelector(
+            ".hamburger")
+        hamburger_container.classList.remove('hamburger-active')
+        hamburger_container.children[0].classList.remove("first-bar")
+        hamburger_container.children[1].classList.remove("middle-bar")
+        hamburger_container.children[2].classList.remove("last-bar")
+    elif document.querySelector(".sidebar-inactive") != None:
+        document.querySelector(
+            ".sidebar").classList.remove("sidebar-inactive")
+        document.querySelector(".shadow").classList.add("shadow-on")
+        hamburger_container = document.querySelector(
+            ".hamburger")
+        hamburger_container.classList.add("hamburger-active")
+        hamburger_container.children[0].classList.add("first-bar")
+        hamburger_container.children[1].classList.add("middle-bar")
+        hamburger_container.children[2].classList.add("last-bar")
+
+
+def shadow_sidebar_toggle():
+    if document.querySelector(".shadow-on") != None:
+        sidebar_toggle()
+
+
+add_event_listener(document.querySelector("body"), "keydown", lambda e: sidebar_toggle(
+) if e.code == "Escape" and document.querySelector(".shadow-on") != None else None)
--- script.py
+++ script.py
@@ -50,33 +50,6 @@ def display_tracked_words() -> None:
         written_words_div.appendChild(new_word)
 
 
-def sidebar_toggle():
-    if document.querySelector(".sidebar-inactive") == None:
-        document.querySelector(
-            ".sidebar").classList.add("sidebar-inactive")
-        document.querySelector(".shadow").classList.remove("shadow-on")
-        hamburger_container = document.querySelector(
-            ".hamburger")
-        hamburger_container.classList.remove('hamburger-active')
-        hamburger_container.children[0].classList.remove("first-bar")
-        hamburger_container.children[1].classList.remove("middle-bar")
-        hamburger_container.children[2].classList.remove("last-bar")
-    elif document.querySelector(".sidebar-inactive") != None:
-        document.querySelector(
-            ".sidebar").classList.remove("sidebar-inactive")
-        document.querySelector(".shadow").classList.add("shadow-on")
-        hamburger_container = document.querySelector(
-            ".hamburger")
-        hamburger_container.classList.add("hamburger-active")
-        hamburger_container.children[0].classList.add("first-bar")
-        hamburger_container.children[1].classList.add("middle-bar")
-        hamburger_container.children[2].classList.add("last-bar")
-
-
-def shadow_sidebar_toggle():
-    if document.querySelector(".shadow-on") != None:
-        sidebar_toggle()
-
 
 def submit_word():
     add_word_input = document.querySelector(".add-word-input")
@@ -110,5 +83,3 @@ rerender_board()
 add_word_input = document.querySelector(".add-word-input")
 add_event_listener(add_word_input, "keydown",
                    lambda e: submit_word() if e.code == "Enter" else None)
-add_event_listener(document.querySelector("body"), "keydown", lambda e: sidebar_toggle(
-) if e.code == "Escape" and document.querySelector(".shadow-on") != None else None)
